31b064f8f3b78a2cf4e137bd99a294b3823a5365
Sapphia <44506268+Sapphia15@users.noreply.github.com>
2026-04-12 15:39:11 -0400

    Updeted argument to handle escape characters

--- lib/argument.lua
+++ lib/argument.lua
@@ -1,18 +1,42 @@
 local argument={}
 
+local function findQuote(s,qt,off,esc)
+  s=string.gsub(s,esc..esc,"["..esc..esc.."]") --isolate escaped escape characters
+  while string.find(s,qt,off) do
+    local qi = string.find(s,qt,off)
+    if qi==off then
+      return qi
+    elseif string.sub(s,qi-string.len(esc),qi-1)==esc and string.sub(s,qi-(2*string.len(esc)),qi-string.len(esc)-1)~=esc then
+      off=qi+1
+    else
+      return qi
+    end
+  end
+end
+
 --doesn't handle escaping quotes
 --set dl to change delimeter (default is space)
 --set qt to change quote character (default is ")
-function argument.parse(s,dl,qt)
+function argument.parse(s,dl,qt,esc)
   dl=dl or " "
   qt=qt or "\""
+  esc=esc or "\\"
   local args={}
-  while string.find(s,dl) do
+  while #s>0 do
     local si=string.find(s,dl)
-    local arg=string.sub(s,1,si-1)
-    if string.sub(s,1,1)==qt and string.find(s,qt,2) then
-      local qi=string.find(s,qt,2)
+    local arg
+    if si then
+      arg=string.sub(s,1,si-1)
+    else
+      arg=s
+    end
+    if string.sub(s,1,1)==qt and findQuote(s,qt,2,esc) then
+      local qi=findQuote(s,qt,2,esc)
       arg=string.sub(s,2,qi-1)
+      --replace \" with " and \\ with \
+      arg=string.gsub(arg,esc..esc,"["..esc..esc.."]") --isolate escaped escape characters from quotes
+      arg=string.gsub(arg,esc.."\"","\"") --replace escaped quotes with quotes
+      arg=string.gsub(arg,"%["..esc..esc.."%]",esc) --replaces isolated escaped escape characters with escape character
       s=string.sub(s,qi)
       si=string.find(s,dl)
     end
